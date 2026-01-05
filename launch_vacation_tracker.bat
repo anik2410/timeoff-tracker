a4fba072e03afb82a9645513183a53cee1d31a70
anik2410 <39357789+anik2410@users.noreply.github.com>
2026-01-05 15:54:32 -0500

    Enhance launch script for Python and environment setup
    Added checks for Python installation, virtual environment creation, and .gitignore file generation.

--- launch_vacation_tracker.bat
+++ launch_vacation_tracker.bat
@@ -1,13 +1,43 @@
-
-cd /d "%~dp0"
-
-:: Activate the virtual environment
-call venv\Scripts\activate.bat
-
+@REM Check if Python is installed
+python --version >nul 2>&1
+IF ERRORLEVEL 1 (
+    echo Python is not installed. Please install Python to continue.
+    pause
+    exit /b 1
+)
+@REM If Python is installed, proceed with the installation
+echo Python is installed.
+:: If a virtual environment already exists, skip creation
+IF NOT EXIST "venv" (
+    echo Creating virtual environment...
+    :: Create a virtual environment
+    python -m venv venv
+    :: Activate the virtual environment
+    call venv\Scripts\activate.bat
+    :: Upgrade pip
+    python -m pip install --upgrade pip
+    :: Install required packages
+    pip install -r requirements.txt
+    echo Installation complete.
+) ELSE (
+    echo Virtual environment already exists. Skipping creation.
+    :: Activate the virtual environment
+    call venv\Scripts\activate.bat
+)
+:: IF .gitignore file does not exist, create it
+IF NOT EXIST ".gitignore" (
+    echo Creating .gitignore file...
+    echo ./ > .gitignore
+    echo __pycache__/ >> .gitignore
+    echo *.pyc >> .gitignore
+    echo *.pyo >> .gitignore
+    echo *.pyd >> .gitignore
+    echo *.db >> .gitignore
+    echo *.txt >> .gitignore
+    echo *.csv >> .gitignore
+    echo venv/ >> .gitignore
+    echo .gitignore file created.
+)
 :: Run the Streamlit app
-start "" streamlit run vacation_tracker.py
-
-:: Keep the window open if something fails
-:: pause
-
-exit
+start "" streamlit run main.py
+exit /b 0
